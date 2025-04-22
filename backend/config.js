a98e95abc0bbee8702fb5bea00563a2df1550c8f
Rodrigo Quiroz <hrqa_20@outlook.com>
2025-04-22 02:31:03 -0500

    Update_host

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
-const DB_HOST = process.env.HOST || 'localhost';
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 5000;
 const DB_DATABASE = process.env.DB_DATABASE || 'db_sv';
 const DB_USER = process.env.DB_USER || 'root';
