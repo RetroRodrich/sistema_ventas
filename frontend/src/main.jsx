87d7747b51b706e16b9c525b4715b70b3c11d075
Rodrigo Quiroz <hrqa_20@outlook.com>
2025-05-29 03:53:29 -0500

    Saludo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/index.css'
-
+// a
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
