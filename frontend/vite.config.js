e3a9050e04b13f54f17101bc27b25f0d0f89231b
Rodrigo Quiroz <hrqa_20@outlook.com>
2025-07-06 02:05:53 -0500

    Prueba Azure 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
   define: {
     __VITE_BACKEND_URL__: JSON.stringify(process.env.VITE_BACKEND_URL)
   }
