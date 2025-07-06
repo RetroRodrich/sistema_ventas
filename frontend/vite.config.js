127ee07b8cfa4870aeaf3345421e853d4d9e9bd6
Rodrigo Quiroz <hrqa_20@outlook.com>
2025-07-06 01:59:18 -0500

    Prueba Azure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __VITE_BACKEND_URL__: JSON.stringify(process.env.VITE_BACKEND_URL)
+  }
 })
